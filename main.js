dd4e55da5fd768a8b84f495bb36cf87adcd11814
Nazarov Yuriy <nazarov.yuriy.pavlovich@gmail.com>
2013-11-11 00:54:55 +0400

    editable hex value

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 /**
  * Created by firefish on 11/9/13.
  */
-function str_inc(str){
-    var len=str.length;
-    var carry = 1;
-    var res = '';
-    for(var i = len-1; i>=0; i-- ){
-        var val = parseInt( str.charAt(i), 16 )+carry;
-        res=(val%16).toString(16) + res;
-        carry = (val>=16)?1:0;
-    }
-    return res;
-}
+angular.module('customControl', []).
+    directive('hexeditable', function() {
+        return {
+            restrict: 'A', // only activate on element attribute
+            require: '?ngModel', // get a hold of NgModelController
+            link: function(scope, element, attrs, ngModel) {
+                if(!ngModel) return; // do nothing if no ng-model
+
+                // Specify how UI should be updated
+                ngModel.$render = function() {
+                    element.val(ngModel.$viewValue.toString(16));
+                };
+
+                // Listen for change events to enable binding
+                element.on('blur keyup change', function() {
+                    scope.$apply(read);
+                });
+                //read(); // initialize
+
+                // Write data to the model
+                function read() {
+                    var val = element.val();
+                    ngModel.$setViewValue(parseInt(val, 16));
+                }
+            }
+        };
+    });
 
 function CpuController($scope) {
     $scope.opcodes = opcodes;
@@ -20,26 +36,26 @@ function CpuController($scope) {
 
     $scope.run_cmd = function(){
         if($scope.instructions_signature.key == 'INC'){
-            $scope.cpu[$scope.instructions_args] = str_inc($scope.cpu[$scope.instructions_args]);
+            $scope.cpu[$scope.instructions_args]++;
         }
     };
 
     var bits = [];
     var bytes = [];
-    var registers = ['rax', 'rbx', 'rcx', 'rdx'];
-    for (var i = 63; i >= 0; i--)
+    for (var i = 31; i >= 0; i--)
         bits.push(i);
-    for (var i = 7; i >= 0; i--)
+    for (var i = 3; i >= 0; i--)
         bytes.push(i);
     $scope.bits = bits;
     $scope.bytes = bytes;
-    $scope.registers = registers;
+    $scope.registers = gp_regs;
     $scope.cpu = cpu_init;
     $scope.get_bit = function (name, bit) {
-        return (   parseInt($scope.cpu[name].substr((14 - 2*(bit >> 3) ), 2), 16) >> (bit % 8)   ) & 1;
+        return ($scope.cpu[name] >> bit) & 1;
     };
     $scope.get_byte = function (name, byte) {
-        return '0x' + $scope.cpu[name].substr((14 - byte * 2), 2);
+        val = (($scope.cpu[name] >> (8*byte)) & 255).toString(16);
+        return (val.length>1 ? '0x' : '0x0')+val;
     };
     $scope.get_class = function (bit) {
         return ((bit >> 3) & 1) ? 'odd_byte' : 'even_byte';
